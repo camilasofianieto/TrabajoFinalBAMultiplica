117cc175083dd2c36a2a22547f8bb71509e21e44
camilasofianieto <nietocamila@outlook.com.ar>
2023-06-14 14:17:17 -0300

    nuevo proyecto

---
+++ script/script.js
@@ -0,0 +1,13 @@
+<script>
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#mailpersonal')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  $buttonMailto.setAttribute('href', `camilanieto10@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+  $buttonMailto.click()
+}
+</script>
